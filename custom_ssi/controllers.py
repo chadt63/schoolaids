c436e0b78d1eae8d0c841911541b3d27ac84c47c
KrisQ <kquemener@yahoo.com>
2018-06-19 13:44:30 -0500

    product func

--- custom_ssi/controllers.py
+++ custom_ssi/controllers.py
@@ -4,11 +4,11 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 
 class CustomCartForms(WebsiteSale):
 
-    # @http.route(['/shop/product/<model("product.template"):product>'], type='http', auth="public", website=True)
-    # def product(self, product, category='', search='', **kwargs):
-    #     r = super(WebsiteSaleOptions, self).product(product, category, search, **kwargs)
-    #     r.qcontext['optional_product_ids'] = map(lambda p: p.with_context({'active_id': p.id}), product.optional_product_ids)
-    #     return r
+    @http.route(['/shop/product/<model("product.template"):product>'], type='http', auth="public", website=True)
+    def product(self, product, category='', search='', **kwargs):
+        r = super(WebsiteSaleOptions, self).product(product, category, search, **kwargs)
+        r.qcontext['optional_product_ids'] = map(lambda p: p.with_context({'active_id': p.id}), product.optional_product_ids)
+        return r
 
     @http.route(['/shop/cart/custom_cart'], type='http', auth="public", methods=['POST'], website=True, multilang=False)
     def cart_update_custom(self, product_iref_1, add_qty_1, product_iref_2, add_qty_2, set_qty=0, goto_shop=None, lang=None, **kw):
@@ -19,12 +19,11 @@ class CustomCartForms(WebsiteSale):
         order = request.website.sale_get_order(force_create=1)
         product = request.env['product.product'].search([('default_code', '=', product_iref_1)], limit=1)
 
-        if product.optional_product_ids:
-            option_ids = product.optional_product_ids.mapped('product_variant_ids').ids
-            optional_product_ids = []
-            for k, v in kw.items():
-                if "optional-product-" in k and int(kw.get(k.replace("product", "add"))) and int(v) in option_ids:
-                    optional_product_ids.append(int(v))
+        option_ids = product.optional_product_ids.mapped('product_variant_ids').ids
+        optional_product_ids = []
+        for k, v in kw.items():
+            if "optional-product-" in k and int(kw.get(k.replace("product", "add"))) and int(v) in option_ids:
+                optional_product_ids.append(int(v))
 
         attributes = self._filter_attributes(**kw)
 
